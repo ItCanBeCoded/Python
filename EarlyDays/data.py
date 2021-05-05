d7770326440129a5c9f45b5428758456583f4f88
itcanbecoded <doherty.steve.j@gmail.com>
2021-05-04 21:07:22 -0500

    band generator program

--- EarlyDays/data.py
+++ EarlyDays/data.py
@@ -1,1 +1,18 @@
-print("Hello github!");
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this: \nprint('what to print')")
+
+# name = input("what is your name? ")
+# chars = len(name)
+# eggo = str(chars)
+# print("Your name has " + eggo + " characters in it")
+
+# a = input("a: ")
+# b = input("b: ")
+
+# print(f"a: {b}")
+# print(f"b: {a}")
+
+print("Welcome to the Band Name Generator")
+city = input("What is the name of the city you grew up in\n")
+pet = input("What is your pet's name?\n")
+print("Your band name could be The " + city + " " + pet + "'s")
